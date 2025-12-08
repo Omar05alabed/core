d040952e44c5aec3c491f84ddd94592a83f211d5
Omar05alabed <158600301+Omar05alabed@users.noreply.github.com>
2025-12-08 15:27:58 +0300

    Add files via upload

---
+++ Common-Core---Libft-957d3607-fc42-44d1-a411-973561401b46/memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memchr.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oalabed <oalabed@learner.42.tech>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 12:40:26 by oalabed           #+#    #+#             */
+/*   Updated: 2025/12/08 12:40:58 by oalabed          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*str;
+
+	str = s;
+	while (n > 0)
+	{
+		if (*str == (unsigned char)c)
+			return ((void *)str);
+		str++;
+		n--;
+	}
+	return (NULL);
+}
---
+++ Common-Core---Libft-957d3607-fc42-44d1-a411-973561401b46/memmove.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memmove.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oalabed <oalabed@learner.42.tech>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 12:04:35 by oalabed           #+#    #+#             */
+/*   Updated: 2025/12/08 12:49:58 by oalabed          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <string.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char		*pdest;
+	const char	*psrc;
+
+	if (!dest && !src)
+		return (NULL);
+	pdest = dest;
+	psrc = src;
+	if (pdest < psrc)
+	{
+		while (n--)
+			*pdest++ = *psrc++;
+	}
+	else
+	{
+		pdest += n;
+		psrc += n;
+		while (n--)
+			*(--pdest) = *(--psrc);
+	}
+	return (dest);
+}
