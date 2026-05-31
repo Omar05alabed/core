fc30eaa731358d6510dcc80e5aaa97fdf62ec0fe
Omar Alabed <158600301+Omar05alabed@users.noreply.github.com>
2026-05-31 18:05:02 +0300

    Add files via upload

--- Common core - Data Quest/ex4/ft_inventory_system.py
+++ Common core - Data Quest/ex4/ft_inventory_system.py
@@ -1,16 +1,49 @@
 import sys
 
+print("=== Inventory System Analysis ===")
+inventory: dict[str, int] = {}
 
-def ft_inventory_system():
-    inventory = {}
-    for item in sys.argv[1]:
+for token in sys.argv[1:]:
+    if token.count(":") != 1:
+        print(f"Error - invalid parameter '{token}'")
+        continue
 
-        if item in inventory.keys():
-            inventory.update({item: inventory[item] + 1})
-        else:
-            inventory.update({item: 1})
+    item, qty = token.split(":")
 
-    print(inventory)
+    if item in inventory:
+        print(f"Redundant item '{item}' - discarding")
+        continue
 
+    try:
+        qty_int = int(qty)
+    except ValueError:
+        print(
+            f"Quantity error for '{item}': "
+            f"invalid literal for int() with base 10: '{qty}'"
+        )
+        continue
 
-ft_inventory_system()
+    inventory[item] = qty_int
+
+total = sum(inventory.values())
+print(f"Got inventory: {inventory}")
+print(f"Item list: {list(inventory.keys())}")
+print(
+    f"Total quantity of the {len(inventory)} items: "
+    f"{sum(inventory.values())}"
+)
+for item, value in inventory.items():
+    print(f"Item {item} represents {round(value/total*100, 1)}%")
+most = max(inventory.values())
+for item, value in inventory.items():
+    if value == most:
+        print(f"Item most abundant: {item} with quantity {value}")
+        break
+least = min(inventory.values())
+for item, value in inventory.items():
+    if value == least:
+        print(f"Item least abundant: {item} with quantity {value}")
+        break
+
+inventory.update({"magic_item": 2})
+print(f"Updated inventory: {inventory}")
